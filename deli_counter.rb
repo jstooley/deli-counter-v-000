211aae7009cc3d922860a62d0fc0a232115c2b91
Justin Tooley <jstooley823@gmail.com>
2017-09-04 23:19:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,16 +2,15 @@
 def line(list)
   if list.lenght == 0
     puts "The line is currently empty."
-    place = 1
   else
-    place = list.lenght +1
+    place = list.lenght 
   end
 end
 
 def take_a_number(list, name)
   list << name
   place = line(list)
-  wait = [name,place]
+  puts "Welcome, #{name}. You are number #{place} in line."
 end
 
 def now_serving(list)
