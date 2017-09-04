4afbaafbd9a660f358b304fdb932a3e8d1e000d3
Justin Tooley <jstooley823@gmail.com>
2017-09-04 23:00:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
 # Write your code here.
+def line(list)
+  if list.lenght == 0 
+    puts "The line is currently empty."
+  else
+    list.each_with_index do |name, i|
+      puts " #{name} is #{i+1} spots back."
+    end
+end
