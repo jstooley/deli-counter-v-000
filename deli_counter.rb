e4840e5d501cb526829b182fc8cad649a31f228f
Justin Tooley <jstooley823@gmail.com>
2017-09-04 23:36:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,11 +12,11 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.lenght} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(list)
-  if katz_deli.lenght == 0
+  if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   else
     puts "#{katz_deli.shift}"
