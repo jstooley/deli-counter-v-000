9d93f29476f5dba028eff22f2b228ea89ab41dcc
Justin Tooley <jstooley823@gmail.com>
2017-09-04 23:03:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,8 @@
 # Write your code here.
 def line(list)
-  if list.lenght == 0 
+  if list.lenght == 0
     puts "The line is currently empty."
   else
-    list.each_with_index do |name, i|
-      puts " #{name} is #{i+1} spots back."
-    end
+    place = list.lenght + 1
+  end
 end
