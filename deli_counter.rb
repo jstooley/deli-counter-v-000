a32460339058e7f67b2fb717253bf9cb9c2e7a8c
Justin Tooley <jstooley823@gmail.com>
2017-09-04 23:29:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,23 +1,23 @@
 # Write your code here.
-def line(list)
-  if list.lenght == 0
+def line(katz_deli)
+  if katz_deli.lenght == 0
     puts "The line is currently empty."
   else
-    list.each do |name|
+    katz_deli.each do |name|
       puts "#{name}"
     end
   end
 end
 
-def take_a_number(list, name)
-  list.push(name)
-  puts "Welcome, #{name}. You are number #{list.lenght} in line."
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.lenght} in line."
 end
 
 def now_serving(list)
-  if list.lenght == 0
+  if katz_deli.lenght == 0
     puts "There is nobody waiting to be served!"
   else
-    puts "#{list.shift}"
+    puts "#{katz_deli.shift}"
   end
 end
